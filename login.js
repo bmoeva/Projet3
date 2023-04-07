713e83125f1b25b2902fdb2cbcf58167f825f654
Bacari Moeva <bacari.moeva@gmail.com>
2023-04-07 04:47:57 +0200

    début du code de la page de connexion

--- login.js
+++ login.js
@@ -1,13 +1,38 @@
-const maGalrie = await fetch('http://localhost:5678/api/works');
+const formEl = document.getElementById('loginform'); console.log(formEl);
 
-{
-    "id": 1,
-    "title": "Abajour Tahina",
-    "imageUrl": "http://localhost:5678/images/abajour-tahina1651286843956.png",
-    "categoryId": 1,
-    "userId": 1,
-    "category": {
-      "id": 1,
-      "name": "Objets"
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const token = localStorage.getItem("token");
+  console.log('token:', token);
+
+  const user = {
+    email: event.target.querySelector("email").value,
+    password: event.target.querySelector("password").value,
+  };
+})
+
+// Création  de la charge utile au format JSON
+const chargeUtile = JSON.stringify(user);
+
+// Appel de la fonction fecth 
+const responseLogin = await fetch('http://localhost:5678/api/users/login', {
+  method: 'POST',
+  headers: {
+    "content-type": "application/json"
+},
+  body: chargeUtile
+})
+  .then(response => {
+    if (response.ok) {
+      document.location.href = "http://127.0.0.1:5501/Portfolio-architecte-sophie-bluel-master/FrontEnd/index.html";
     }
-}
+    else {
+      const erreur = document.createElement('p');
+      erreur.textContent = 'erreur d authentification';
+      erreur.className = "red";
+      Form.appendChild(erreur);
+    }
+  })
+//const loginPage = await responseLogin.json();
+//console.log(responseLogin);
