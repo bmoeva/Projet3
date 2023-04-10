8bcc475ede88035084e458eeb4ebd4c8e2b21200
Bacari Moeva <bacari.moeva@gmail.com>
2023-04-10 13:59:45 +0200

    travail sur la page de connexion

--- login.js
+++ login.js
@@ -1,32 +1,49 @@
-const user = document.getElementById('#login');
+init();
 
-user.addEventListener('submit', event => {
+async function init() {
+
+const loginForm = document.getElementById('login');
+
+loginForm.addEventListener('submit', event => {
   event.preventDefault();
+  event.stopImmediatePropagation();
+console.log(event);
+  
 
-  const token = localStorage.getItem("token"); // Stockage du token d'authentification
-  console.log('token:', token);
+  //const token = localStorage.getItem("token"); // Stockage du token d'authentification
+  //console.log('token:', token);
 
   const idConnexion = {
-    email: event.target.querySelector("email").value,
-    password: event.target.querySelector("password").value,
-  }
-});
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+    
+  };
+// Appel de la fonction fecth 
+const responseLogin = fetch('http://localhost:5678/api/users/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',  
+},
+  body: JSON.stringify(idConnexion)
+})
+
+
+/*then(response => {
+  if (response.ok) {
+    alert("Bienvenu Sophie Bluel");
+    console.log(response);
+}})*/
+})
+
+
 
 // Création  de la charge utile au format JSON
 //const chargeUtile = JSON.stringify(user);
 
-// Appel de la fonction fecth 
-const response = await fetch('http://localhost:5678/api/users/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-},
-  body: JSON.stringify(user)
-  //body: chargeUtile
-});
 
-let reuslt = await response.json();
-alert(reuslt.message); console.log(reuslt);
+
+//let reuslt = response.json();
+//alert(reuslt.message); console.log(reuslt);
 
 // Rédirection ver la page d'acceuil
   /*.then(response => {
@@ -42,3 +59,4 @@ alert(reuslt.message); console.log(reuslt);
       Form.appendChild(erreur);
     }
   })*/
+}
