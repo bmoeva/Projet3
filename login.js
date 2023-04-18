eed3c83919695d0ba84b874a7cd2c95f6e053d3f
Bacari Moeva <bacari.moeva@gmail.com>
2023-04-18 15:11:42 +0200

    Suppression d'élements non nécessaire dans login.js, ajout du début CSS de la modal.

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ loginForm.addEventListener('submit', event => {
 const idConnexion = {
     email: document.getElementById("email").value,
     password: document.getElementById("password").value,
-  };
+};
   console.log(idConnexion);
 
 // Appel de la fonction fetch 
@@ -27,83 +27,23 @@ body: JSON.stringify(idConnexion),
 
 })
   .then(response => response.json())
-  .then((data) => {  
+  .then((data) =>  {  
+    alert("Vous êtes connecté !");
     localStorage.setItem("token", data.token),
-    window.location.href = "index.html";
+    window.location.href = "index.html"; //Redirection à la page d'acceuil
     console.log(data);
 
-    if(response.ok) {
+  if(response.ok) {
       console.log("Connexion réussie !");
       return response.json();
-    }
-    else{
-      console.error("Impossible de ce connecter !");
-    }
-  }) 
+}
+  else{
+    console.error("Impossible de ce connecter !");
+}
+}) 
 
   .catch((error) => {
-    console.log(error);
+   console.log(error);
 });
-
-
-  //
-  //if(response.ok === true) {
-    //alert("Vous êtes connectés !")
-  //}
-
-  //else {
-    //throw new Error('Il y a une erreur !')
-  //}; console.log(response.ok);
 }); 
 }
-
-
-//else {
-  //error.innerHTML =""
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*.then((responseData) => {
-  data = responseData;
-  console.log(data);
-  if (response.ok) {
-    alert('Vous êtes connectées !');
-    console.log(idConnexion);
-  }
-});*/
-
-// Création  de la charge utile au format JSON
-//const chargeUtile = JSON.stringify(user);
-
-//const token = localStorage.getItem("token"); // Stockage du token d'authentification
-  //console.log('token:', token);
-
-//let reuslt = response.json();
-//alert(reuslt.message); console.log(reuslt);
-
-// Rédirection ver la page d'acceuil
-  /*.then(response => {
-    if (response.ok) {
-      window.location.href = "http://127.0.0.1:550//Portfolio-architecte-sophie-bluel-master/FrontEnd/index.html";
-    }
-
-    // Prévenir l'utilaseur avec un message d'erreur 
-    else {
-      const erreur = document.createElement('p');
-      loginError.textContent = 'erreur d'\ authentification';
-      erreur.className = "red";
-      Form.appendChild(erreur);
-    }
-  })*/
