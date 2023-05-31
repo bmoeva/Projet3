ff59190666ce43b6eab917a6c11135cda85587ee
Bacari Moeva <bacari.moeva@gmail.com>
2023-05-31 14:58:18 +0200

    Ajout du messge d'erreur lorsque le formulaire login est mal rempli

--- login.js
+++ login.js
@@ -1,8 +1,14 @@
-init();
-async function init() {
 
-const loginForm = document.getElementById('login');
-loginForm.addEventListener('submit', event => {
+init();
+
+async function init() {
+  const inputEmail = document.getElementById("email");
+  const inputPassword = document.getElementById("password");
+  const feedbackPassword = document.getElementById('feedback-password');
+  const feedbackEmail = document.getElementById('feedback-email');
+  const loginForm = document.getElementById('login');
+  loginForm.addEventListener('submit', event => {
+ 
   event.preventDefault();
   event.stopImmediatePropagation();
   console.log(event);
@@ -13,12 +19,6 @@ const idConnexion = {
 };
   console.log(idConnexion);
 
-  const inputEmail = document.getElementById("email");
-  const inputPassword = document.getElementById("password");
-
-  const emailError = document.getElementById("email-error");
-  const passwordError = document.getElementById("password-error");
-
 // Appel de la fonction fetch 
 fetch('http://localhost:5678/api/users/login', {
 
@@ -26,64 +26,39 @@ method: 'POST',
 headers: {
   'Content-Type': 'application/json',
 },
+
 body: JSON.stringify(idConnexion),
-})
-
-/*.then((response) => response.json())
-.then((respsonseData) => {
-  data = respsonseData;
-  console.log(data);
-});
-if (responseData.message) {
-  emailError.textContent = "Erreur dans l'identifiant !";
-  email.style.color = "red";
-  console.log(idConnexion);
-}
-else if (data.error) {
-  passwordError.textContent = "Une erreur dans le mot de passe !";
-  emailError.textContent = "";
-  inputPassword.style.color= "red";
-  inputEmail.style.color = "#1d6154";
-  console.log(idConnexion);
-}
-else {
-  inputPassword.style.color = "#1d6154";
-  passwordError.textContent = "";
-  emailError.textContent = "";
-  console.log("LogAdmon OK");
-  console.log(idConnexion);
-
-  localStorage.setItem("token", data.token),
-  window.location.href = "index.html"; //Redirection à la page d'acceuil
-}*/
-  .then(response => {
-    if (response.ok) {
-      return(response.json());
-    } 
-    else  {
-      //alert("Impossible de ce connecter ! Veuillez vérifier vos identifiants !");
-      passwordError.textContent = "Une erreur dans le mot de passe !";
-      emailError.textContent = "";
-      inputPassword.style.color= "red";
-      inputEmail.style.color = "#1d6154";
-      console.log(idConnexion);
-    }
 
 })
+.then(response => {
+  if (response.ok) {
+    return(response.json());
+  }
+
+else  {
+    feedbackEmail.textContent = "Erreur dans l’identifiants !";
+    inputEmail.style.color = "red";
+    feedbackEmail.style.color = "red";
+
+} if (response.message) {
+
+    feedbackPassword.textContent = "Erreur dans le mot de passe !" ;
+    feedbackEmail.textContent = "";
+    inputPassword.style.color = "red";
+    inputEmail.style.color = "#1d6154" ;
+
+}
+
+  }) 
   .then((data) =>  {  
-
-    //inputPassword.style.color = "#1d6154";
-    //passwordError.textContent = "";
-    //emailError.textContent = "";
-
     localStorage.setItem("token", data.token),
     window.location.href = "index.html"; //Redirection à la page d'acceuil
     console.log(data);
-    
-}
-)
-}
-);
-}; 
+
+    inputPassword.style.color = "#1d6154";
+    feedbackPassword.textContent = "";
+    feedbackEmail.textContent = "";
+  });
+})}
 
 
